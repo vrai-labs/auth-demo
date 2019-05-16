1bfa49f2f9bc55ce15f34bf98a60215018b397c1
Rishabh Poddar <rishabh.poddar@gmail.com>
2019-05-16 17:58:50 +0530

    implements skeleton for frontend application

---
+++ frontend/build/index.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+class Router extends React.PureComponent {
+    render() {
+        return null;
+    }
+}
+ReactDOM.render((React.createElement(Router, null)), document.getElementById("root"));
---
+++ frontend/ts/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+class Router extends React.PureComponent<{}, {}> {
+    render() {
+        return null;
+    }
+}
+
+ReactDOM.render((
+    <Router />
+), document.getElementById("root"));
